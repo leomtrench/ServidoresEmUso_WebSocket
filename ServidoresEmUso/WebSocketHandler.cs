915b97a935b34c202037226ada776d27b2034a42
GRT Solucoes <devgrtsolucoes@gmail.com>
2020-07-31 03:05:20 -0300

    [FIX] Correção de inconsistencia quando era encerrado uma conexão sendo que a mensagem estava sendo enviada para a lista de conexões.

--- ServidoresEmUso/WebSocketHandler.cs
+++ ServidoresEmUso/WebSocketHandler.cs
@@ -29,7 +29,7 @@ namespace ServidoresEmUso
         public static async Task Echo(WebSocket webSocket)
         {
             byte[] buffer = new byte[1024 * 4];
-            
+
             buffer = await SendMessageAsync(webSocket, ServidorBusiness.Instance.JsonServidores);
 
             var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
@@ -38,22 +38,60 @@ namespace ServidoresEmUso
             {
                 result = await HandleReceivedMessageAsync(webSocket, buffer, result);
             }
-            ActiveWebSockets.Remove(webSocket);
+
+            await RemoveFromActiveWebSockets(webSocket);
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
 
+        private static async Task RemoveFromActiveWebSockets(WebSocket webSocket)
+        {
+            await SemaphoreSlim.WaitAsync();
+            try
+            {
+                ActiveWebSockets.Remove(webSocket);
+            }
+            finally
+            {
+                SemaphoreSlim.Release();
+            }
+        }
+
+        static SemaphoreSlim SemaphoreSlim = new SemaphoreSlim(1, 1);
         private static async Task<WebSocketReceiveResult> HandleReceivedMessageAsync(WebSocket webSocket, byte[] buffer, WebSocketReceiveResult result)
         {
             var receivedMessage = System.Text.Encoding.UTF8.GetString(buffer, 0, result.Count);
 
             ServidorBusiness.Instance.ChangeStatusServidor(receivedMessage);
+            await SendMessageToActiveWebSockets();
 
-            foreach (var activeWebSocket in ActiveWebSockets)
-            {
-                await SendMessageAsync(activeWebSocket, ServidorBusiness.Instance.JsonServidores);
-            }
             result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             return result;
+        }
+
+        private static async Task SendMessageToActiveWebSockets()
+        {
+            await SemaphoreSlim.WaitAsync();
+            try
+            {
+                List<WebSocket> webSocketsWithError = new List<WebSocket>();
+                foreach (var activeWebSocket in ActiveWebSockets)
+                {
+                    try
+                    {
+                        await SendMessageAsync(activeWebSocket, ServidorBusiness.Instance.JsonServidores);
+                    }
+                    catch (Exception ex)
+                    {
+                        webSocketsWithError.Add(activeWebSocket);
+                    }
+                }
+                ActiveWebSockets.RemoveAll(x => webSocketsWithError.Contains(x));
+                webSocketsWithError.Clear();
+            }
+            finally
+            {
+                SemaphoreSlim.Release();
+            }
         }
 
         private static async Task<byte[]> SendMessageAsync(WebSocket webSocket, string message)
